855a8635334d29cae20bba26d21f2ca164bdf770
mustangislife <146365892+mustangislife@users.noreply.github.com>
2023-10-01 15:48:15 +0530

    Create simple_interest.c
    Calculate a Simple Interest

---
+++ simple_interest.c
@@ -0,0 +1,23 @@
+//Calculate a Simple Interest
+#include <stdio.h>
+int main(void)
+{
+    int p; //principal amount
+    float r; //rate of interest
+    float t; //time
+    float si; //simple interest
+
+    printf("Enter the principal amount : ");
+    scanf("%d", &p);
+
+    printf("Enter the rate of interest : ");
+    scanf("%f", &r);
+
+    printf("Enter the time : ");
+    scanf("%f", &t);
+
+    si = (p * r * t)/100;
+    printf("The simple interest for the given data is : %f\n", si);
+
+    return 0;
+}
