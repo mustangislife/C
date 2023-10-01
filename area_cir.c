b879f3052bd499d68433c62e34ade87c65e66c8a
mustangislife <146365892+mustangislife@users.noreply.github.com>
2023-10-01 15:12:20 +0530

    Create area_cir.c
    C Program to Find Area and Circumference of Circle

---
+++ area_cir.c
@@ -0,0 +1,24 @@
+//C Program to Find Area and Circumference of Circle
+
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159265358979323846
+
+int main(void)
+{
+    float radius;
+    float area, cir;
+
+    printf("Enter the radius of the circle : ");
+    scanf("%f", &radius);
+
+    //for area
+    area = PI * radius * radius;
+    printf("The area of the circle is : %f\n", area);
+
+    //for circumference
+    cir = 2 * PI * radius;
+    printf("The circumference of the circle is : %f\n", cir);
+
+    return 0;
+}
